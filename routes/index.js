b78fd866ced70f7c4558b915c45b2bbd2b79728d
Diego Batista <fernandez.df@gmail.com>
2018-04-04 16:35:27 -0300

    demo2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node on blumix ;) test' });
+  res.render('index', { title: 'Node on blumix ;)' });
 });
 
 module.exports = router;
